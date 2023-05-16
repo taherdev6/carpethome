d2e4bfeabae1fb8a620f72c493765273efc527c3
taherdev6 <taherdev6@gmail.com>
2023-05-16 21:37:44 +0300

    removed firebase

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,38 +1,36 @@
 import React, { useState } from 'react'
-import { useEffect } from 'react'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
-import firebase from 'firebase/compat/app'
-import { useUserContext } from '../context/user_context'
-import { Navigate } from 'react-router-dom'
-import { getAuth, sendEmailVerification } from "firebase/auth";
+// import { useEffect } from 'react'
+// import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+// import firebase from 'firebase/compat/app'
+// import { useUserContext } from '../context/user_context'
+// import { Navigate } from 'react-router-dom'
 
 
-  const uiConfig = {
-     // Popup signin flow rather than redirect flow.
-  signInFlow: 'redirect',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ],
-  }
+  // const uiConfig = {
+  //    // Popup signin flow rather than redirect flow.
+  // signInFlow: 'redirect',
+  // // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  // signInSuccessUrl: '/',
+  // signInOptions: [
+  //   firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  // ],
+  // }
 const LoginPage = () => {
-  if(localStorage.getItem('pending')) localStorage.removeItem('pending') 
-    const {setMyUser, myUser, app} = useUserContext();
-    const [message, setMessage] = useState('')
-    const auth = getAuth();
+  // if(localStorage.getItem('pending')) localStorage.removeItem('pending') 
+  //   const {setMyUser, myUser, app} = useUserContext();
+  //   const [message, setMessage] = useState('')
   
     
-    if(myUser) {
-        return <Navigate to='/'/>
-    }
-  return (
-    <div>
-          <h4>{message}</h4>
-          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+  //   if(myUser) {
+  //       return <Navigate to='/'/>
+  //   }
+  // return (
+  //   <div>
+  //         <h4>{message}</h4>
+  //         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
 
-    </div>
-  )
+  //   </div>
+  // )
 }
 
 export default LoginPage
